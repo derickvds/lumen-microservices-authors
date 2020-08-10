97a22775078a0860e620c295ba35d8bccb65ce2f
Derick van der Sandt <derickvds@gmail.com>
2020-08-10 12:07:49 +0200

    Added author routes and functions

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Return full list of authors
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Create a new author
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * Obtain and show author
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function show($author)
+    {
+
+    }
+
+    /**
+     * Update an existing author
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function update(Request $request, $author)
+    {
+
+    }
+
+    /**
+     * Delete an existing author
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function destroy($author)
+    {
+
+    }
+
+
+    //
+}
